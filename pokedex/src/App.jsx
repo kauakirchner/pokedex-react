23fdeac1c608d7276d9bf6dcb9df402f29730a3c
Kaua <kauakirchner2@gmail.com>
2022-08-23 21:29:00 -0300

    Create React app and install dependencies

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Pokedex from './components/pages/Pokedex'
+
+function App() {
+  return (
+    <div>
+      <Pokedex />
+    </div>
+  )
+}
+
+export default App
